82347afa3aa8dbbbdee6574d6b97254a72deaa83
yisenhuang <1501919885@qq.com>
2023-09-05 16:27:30 +0800

    初始化项目

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: '新旦智能',
+    description: 'The best place to start your documentation.11',
+    url: 'http://docus.dev',
+    titleTemplate: '新旦智能',
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
@@ -29,10 +30,11 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
+      title: "新旦智能",
       logo: true,
-      showLinkIcon: true,
       exclude: [],
-      fluid: true
+      showLinkIcon: true,
+      fluid: false
     }
   }
 })
---
+++ components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+    <img src="/img/logo.png" />
+</template>
+<style>
+img {
+    width: 60px;
+    height: 100% !important;
+    aspect-ratio: 1/1;
+    opacity: 1;
+}
+</style>
